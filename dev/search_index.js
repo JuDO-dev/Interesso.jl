3d258f7e9d0051809bb035286e73a9a43f053483
Documenter.jl <documenter@juliadocs.github.io>
2022-04-04 16:55:57 +0000

    build based on 22304f5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Interesso","category":"page"},{"location":"#Interesso","page":"Home","title":"Interesso","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Interesso.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Interesso]","category":"page"}]
+}
